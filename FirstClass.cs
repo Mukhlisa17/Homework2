fa578e955b85d7c36b1c859cd83806b70fa89d2f
Brad Traversy <muxlisaxoliyorova@gmail.com>
2022-08-28 23:14:31 +0500

    Добавьте файлы проекта.

---
+++ FirstClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    public class FirstClass
+    {
+        public static void Main(string[] args)
+        {
+            int names = 3;
+
+            Person[] pr = new Person[names];
+
+            for(int i=0; i< names; i++)
+            {
+                pr[i] = new Person()
+                {
+                    Name = Console.ReadLine()
+                };
+            }
+        for(int i=0; i<names; i++)
+            {
+                Console.WriteLine(pr[i].ToString());
+            }
+            Console.ReadLine();
+        }
+       
+    }
+
+
+    
+}
+
---
+++ Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    public  class Person
+    {
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return "Hello, My name is" + Name;
+        }
+    }
+}
